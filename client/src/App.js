4e0fc07d06de94de6595a6519a4d0709311b3ed6
Eric Taylor <erc.tylr@gmail.com>
2018-08-22 23:19:40 -0400

    updating app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { data: [] }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch('/api/tracker')
+      .then(res => res.json())
+      .then(data => this.setState({ data }));
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="root">
+          {data[0]}
       </div>
     );
   }
