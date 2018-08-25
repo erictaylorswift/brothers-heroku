458246fffc842ce4012af382ed1f27ff139a963e
Eric Taylor <erc.tylr@gmail.com>
2018-08-24 23:50:52 -0400

    reverting

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import KdChart from './components/bar-chart';
 import SimpleAppBar from './components/appbar';
-<<<<<<< HEAD
-=======
-// import UserCard from './components/user-list';
->>>>>>> parent of 591c115... adding back
+import UserCard from './components/user-list';
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { theme } from './theme.js'
 
@@ -27,16 +24,11 @@ class App extends Component {
       <div>
         <SimpleAppBar/>
         <div class="mainSection">
-<<<<<<< HEAD
-          <h3>Lifetime KD Scores</h3>
-          <KdChart data={stats}/>
-=======
           <h2>The Brothers</h2>
           <UserCard data={stats}/>
           <div class='container'>
-            // <KdChart data={stats}/>
+            <KdChart data={stats}/>
           </div>
->>>>>>> parent of 591c115... adding back
         </div>
       </div>
       </MuiThemeProvider>
