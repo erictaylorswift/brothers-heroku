4f926ec7175500f823276612046ada8a109709cd
Eric Taylor <erc.tylr@gmail.com>
2018-08-24 23:48:24 -0400

    Revert "adding back"
    This reverts commit 591c115320ad8a07e46b7ef1d8c5e9d9afd8372b.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import KdChart from './components/bar-chart';
 import SimpleAppBar from './components/appbar';
+<<<<<<< HEAD
+=======
+// import UserCard from './components/user-list';
+>>>>>>> parent of 591c115... adding back
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { theme } from './theme.js'
 
@@ -23,8 +27,16 @@ class App extends Component {
       <div>
         <SimpleAppBar/>
         <div class="mainSection">
+<<<<<<< HEAD
           <h3>Lifetime KD Scores</h3>
           <KdChart data={stats}/>
+=======
+          <h2>The Brothers</h2>
+          <UserCard data={stats}/>
+          <div class='container'>
+            // <KdChart data={stats}/>
+          </div>
+>>>>>>> parent of 591c115... adding back
         </div>
       </div>
       </MuiThemeProvider>
