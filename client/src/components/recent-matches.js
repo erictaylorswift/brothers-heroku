ebfddbf6297d1701b93db2109557630aceaea8f4
Eric Taylor <erc.tylr@gmail.com>
2018-08-30 22:29:34 -0400

    added timestamp

--- client/src/components/recent-matches.js
+++ client/src/components/recent-matches.js
@@ -12,11 +12,39 @@ class RecentMatch extends Component {
 
         if (stats){
           for (var i = 0; i < stats.length; i++){
+
+            function addZero(m) {
+              if ( m < 10 ) {
+                m = "0" + m;
+              }
+              return m;
+            }
+
+            function est(tz) {
+              if ( tz < 12 ) {
+                tz = (tz + 12) - 4;
+              }
+              return tz
+            }
+
+            function twelveHour(h) {
+              if ( h > 12 ) {
+                h = h - 12;
+              }
+              return h;
+            }
+
             let matches = stats[i].recentMatches[0].playlist;
             let kills = stats[i].recentMatches[0].kills;
             let minutes = stats[i].recentMatches[0].minutesPlayed;
             let score = stats[i].recentMatches[0].score;
             let winner = stats[i].recentMatches[0].top1;
+            let gameDate = new Date(stats[i].recentMatches[0].dateCollected);
+            let gameEST = est(gameDate.getHours());
+            let gameHours = twelveHour(gameEST);
+            let gameMin = addZero(gameDate.getMinutes());
+            let gameTime = gameHours + ":" + gameMin + " EST";
+            let gameDay = gameDate.toString().split(" ")[0];
 
             let results =
               <div className='user-wrapper'>
@@ -24,6 +52,10 @@ class RecentMatch extends Component {
                 <div className='recent-card'>
                 {winner > 0 ? <Icon icon={trophy} /> : <Icon icon={bin} />}
                   <div >
+                    <p>
+                      {gameDay + " at " + gameTime}
+                    </p>
+                    <hr className='recentLine' /><br/>
                     <p>
                       Your last match, you spent {minutes} min. playing {matches === 'p10' ? 'Duos' : (matches === 'p9' ? 'Squads' : 'Solos')}.
                       <br/>You got {kills} kills and scored {score} points.
@@ -33,7 +65,7 @@ class RecentMatch extends Component {
                 </div>
               </div>
             recent.push(results);
-            console.log(matches)
+            console.log(gameDay.split(" ")[0])
           }
 
         };
